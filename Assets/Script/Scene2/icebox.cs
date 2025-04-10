0aa13675b73d7d818098ba2bab591ad28c6cc136
viggggil <max858807@163.com>
2025-04-10 21:14:57 +0800

    1

---
+++ Assets/Script/Scene2/icebox.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class icebox : MonoBehaviour
+{
+    public GameObject icebox1;
+    public GameObject icebox2;
+    public GameObject fatMirror;
+    public bool flag;
+    private void Start()
+    {
+        flag = false;
+    }
+    private void OnMouseDown()
+    {
+        if (!flag)
+        {
+            flag = true;
+            icebox1.SetActive(false);
+            icebox2.SetActive(true);
+            fatMirror.SetActive(true);
+            fatMirror.transform.DOMove(fatMirror.transform.position+new Vector3(0,0.5f,0), 1.0f);
+        }
+    }
+}
